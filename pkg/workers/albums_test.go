e4c113fece0f6d967a8bcfdb0c1fecedd39acfc7
Natasha Audrey <nat.a.yocum@gmail.com>
2026-09-11 19:37:37 -0700

    test: add albums worker tests (#49)

---
+++ pkg/workers/albums_test.go
@@ -0,0 +1,85 @@
+package workers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestAlbumsParse_err(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{`))
+	}))
+	t.Cleanup(server.Close)
+
+	req, err := http.NewRequest("GET", server.URL, nil)
+	if err != nil {
+		t.Fatal("Error talking to server")
+	}
+	response, err := server.Client().Do(req)
+	if err != nil {
+		t.Fatal("Error talking to server")
+	}
+
+	albums, err := Albums{}.Parse(response)
+	if albums != nil && err != nil {
+		t.Fatal("Albums and error should be nil")
+	}
+
+}
+
+func TestAlbumsParse_success(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`
+{
+	"topalbums": {
+		"album": [
+			{
+				"name": "album_name",
+				"playcount": "32",
+				"artist": {
+					"name": "artist_name"
+				},
+				"image": [
+					{"size": "small", "#text": "url"},
+					{"size": "medium", "#text": "url"},
+					{"size": "large", "#text": "url"},
+					{"size": "extralarge", "#text": "url"}
+				]
+			},
+			{
+				"name": "album_name",
+				"playcount": "31",
+				"artist": {
+					"name": "artist_name"
+				},
+				"image": [
+					{"size": "small", "#text": "url.png"},
+					{"size": "medium", "#text": "url.png"},
+					{"size": "large", "#text": "url.png"},
+					{"size": "extralarge", "#text": "url.png"}
+				]
+			}
+		]
+	}
+}
+		`))
+	}))
+	defer server.Close()
+
+	req, err := http.NewRequest("GET", server.URL, nil)
+	if err != nil {
+		t.Fatal("Error talking to server")
+	}
+	response, err := server.Client().Do(req)
+	if err != nil {
+		t.Fatal("Error talking to server")
+	}
+
+	albums, err := Albums{}.Parse(response)
+	if len(albums) != 2 {
+		t.Fatal("Not expected number of albums")
+	}
+}
